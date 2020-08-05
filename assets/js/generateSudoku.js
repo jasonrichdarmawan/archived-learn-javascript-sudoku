8626acbc8403d4f704a72bb9e8971fd8c82a3aa9
kidfrom <jason.onggo@tempatkerja.com>
2020-08-05 19:49:20 +0700

    add new problem that can't be solved by the Swap, Sort, Shrink strategy

--- assets/js/generateSudoku.js
+++ assets/js/generateSudoku.js
@@ -85,7 +85,7 @@ function populateTheNestedArray() {
   //   [3,1,1,4]
   // ];
 
-  // debug only: ERROR vertical steps 8 needs new strategy
+  // debug only: ERROR vertical steps 8 needs new strategy || Fixed
   // subGridRowLength = 3;
   // gridNestedArray = [
   //     [5, 8, 3, 1, 9, 6, 7, 2, 8],
@@ -99,13 +99,27 @@ function populateTheNestedArray() {
   //     [4, 5, 9, 8, 6, 3, 1, 7, 2]
   // ];
 
-  // // debug only: ERROR vertical steps 2 needs new strategy
+  // debug only: ERROR vertical steps 2 needs new strategy
   // subGridRowLength = 2;
   // gridNestedArray = [
   //   [3, 2, 3, 1],
   //   [4, 1, 2, 4],
   //   [3, 4, 3, 1],
   //   [1, 2, 2, 4]
+  // ];
+  
+  // debug only: ERROR vertical steps 1 needs new strategy
+  // subGridRowlength = 2;
+  // gridNestedArray = [
+  //     [8,4,5,4,6,3,1,6,9],
+  //     [3,1,2,9,8,7,4,8,3],
+  //     [9,6,7,1,5,2,5,2,7],
+  //     [2,3,4,6,7,3,1,9,8],
+  //     [8,1,6,9,4,5,2,5,4],
+  //     [7,5,9,8,1,2,7,3,6],
+  //     [2,9,5,4,9,8,3,2,8],
+  //     [7,1,3,3,1,7,4,6,5],
+  //     [6,4,8,5,6,2,9,7,1]
   // ];
 
   // debug only
